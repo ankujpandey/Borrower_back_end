5f6d882a18740bbd141c3756f1d9786b429bbf3a
ankujpandey <pandeyankuj@gmail.com>
2023-03-29 18:49:10 +0530

    made some changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ApiRoutes = require("./routes");
+const cors = require("cors");
+
 const PORT = 4000;
 
 const setupAndStartServer = async () => {
 	//create the express object
 	const app = express();
+	app.use(cors());
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: true }));
 
