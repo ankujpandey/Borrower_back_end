941da5f484a57f29ed69b27a72e6fd40d9f71db4
ankujpandey <pandeyankuj@gmail.com>
2023-04-26 15:27:58 +0530

    solved some errors'

--- src/migrations/20230328063044-create-loan-details.js
+++ src/migrations/20230328063044-create-loan-details.js
@@ -1,65 +1,65 @@
 "use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("loan_details", {
-      LoanId: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      uid: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        onDelete: "CASCADE",
-        references: {
-          model: "users",
-          key: "uid",
-        },
-      },
-      jobAssignees_id: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-      },
-      amount: {
-        allowNull: true,
-        type: Sequelize.STRING,
-      },
-      rate_of_interest: {
-        allowNull: true,
-        type: Sequelize.STRING,
-      },
-      tenure: {
-        allowNull: true,
-        type: Sequelize.STRING,
-      },
-      createdby: {
-        allowNull: false,
-        type: Sequelize.STRING,
-        defaultValue: "user",
-      },
-      updatedby: {
-        allowNull: false,
-        type: Sequelize.STRING,
-        defaultValue: "user",
-      },
-      isDeleted: {
-        allowNull: false,
-        type: Sequelize.BOOLEAN,
-        defaultValue: false,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("loan_details");
-  },
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("loan_details", {
+			LoanId: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			uid: {
+				allowNull: false,
+				type: Sequelize.INTEGER,
+				onDelete: "CASCADE",
+				references: {
+					model: "users",
+					key: "uid",
+				},
+			},
+			jobAssignees_id: {
+				allowNull: false,
+				type: Sequelize.INTEGER,
+			},
+			amount: {
+				allowNull: true,
+				type: Sequelize.STRING,
+			},
+			rate_of_interest: {
+				allowNull: true,
+				type: Sequelize.STRING,
+			},
+			tenure: {
+				allowNull: true,
+				type: Sequelize.STRING,
+			},
+			createdBy: {
+				allowNull: false,
+				type: Sequelize.STRING,
+				defaultValue: "user",
+			},
+			updatedBy: {
+				allowNull: false,
+				type: Sequelize.STRING,
+				defaultValue: "user",
+			},
+			isDeleted: {
+				allowNull: false,
+				type: Sequelize.BOOLEAN,
+				defaultValue: false,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable("loan_details");
+	},
 };
--- src/models/employment_details.js
+++ src/models/employment_details.js
@@ -1,76 +1,76 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class employment_details extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      this.belongsTo(models.users, {
-        foreignKey: "uid",
-        onDelete: "CASCADE",
-        onUpdate: "CASCADE",
-      });
-    }
-  }
-  employment_details.init(
-    {
-      Empid: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER,
-      },
-      uid: {
-        unique: true,
-        allowNull: false,
-        type: DataTypes.INTEGER,
-      },
-      employment_type: {
-        allowNull: true,
-        type: DataTypes.STRING,
-      },
-      company_name: {
-        allowNull: true,
-        type: DataTypes.STRING,
-      },
-      email: {
-        allowNull: true,
-        type: DataTypes.STRING,
-      },
+	class employment_details extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			this.belongsTo(models.users, {
+				foreignKey: "uid",
+				onDelete: "CASCADE",
+				onUpdate: "CASCADE",
+			});
+		}
+	}
+	employment_details.init(
+		{
+			Empid: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: DataTypes.INTEGER,
+			},
+			uid: {
+				unique: true,
+				allowNull: false,
+				type: DataTypes.INTEGER,
+			},
+			employment_type: {
+				allowNull: true,
+				type: DataTypes.STRING,
+			},
+			company_name: {
+				allowNull: true,
+				type: DataTypes.STRING,
+			},
+			professional_email: {
+				allowNull: true,
+				type: DataTypes.STRING,
+			},
 
-      business_nature: {
-        allowNull: true,
-        type: DataTypes.STRING,
-      },
-      monthly_income: {
-        allowNull: true,
-        type: DataTypes.STRING,
-      },
+			business_nature: {
+				allowNull: true,
+				type: DataTypes.STRING,
+			},
+			monthly_income: {
+				allowNull: true,
+				type: DataTypes.STRING,
+			},
 
-      createdby: {
-        allowNull: false,
-        type: DataTypes.STRING,
-        defaultValue: "user",
-      },
-      updatedby: {
-        allowNull: false,
-        type: DataTypes.STRING,
-        defaultValue: "user",
-      },
-      isDeleted: {
-        allowNull: false,
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-      },
-    },
-    {
-      sequelize,
-      modelName: "employment_details",
-    }
-  );
-  return employment_details;
+			createdBy: {
+				allowNull: false,
+				type: DataTypes.STRING,
+				defaultValue: "user",
+			},
+			updatedBy: {
+				allowNull: false,
+				type: DataTypes.STRING,
+				defaultValue: "user",
+			},
+			isDeleted: {
+				allowNull: false,
+				type: DataTypes.BOOLEAN,
+				defaultValue: false,
+			},
+		},
+		{
+			sequelize,
+			modelName: "employment_details",
+		}
+	);
+	return employment_details;
 };
