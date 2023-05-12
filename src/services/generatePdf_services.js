7fccb23f915e3cc9f49e28163451d6a38ebe7c67
ankujpandey <pandeyankuj@gmail.com>
2023-05-12 18:37:19 +0530

    change in pdf download

--- src/services/generatePdf_services.js
+++ src/services/generatePdf_services.js
@@ -1,44 +1,29 @@
-const { Users_repository } = require("../repository");
+const { Users_repository, Generate_Pdf } = require("../repository");
 const { getImages } = require("../mongodb/kyc_image");
 const pdfTemplate = require("./template/pdfTemplate");
 const puppeteer = require("puppeteer");
 
 class GeneratePdf_service {
-  constructor() {
-    this.usersRepository = new Users_repository();
-  }
-  // -----------------------------------
-  // insert into table
-  // -----------------------------------
-  async generatedpdfService(id) {
-    console.log("service==============", id);
-    const UserData = await this.usersRepository.getAllData(id);
-    const imageData = await getImages(id);
-    console.log("user============", UserData);
-    console.log("image==============", imageData[0].profile_photo);
+	constructor() {
+		this.usersRepository = new Users_repository();
+		this.generetePDF = new Generate_Pdf();
+	}
+	// -----------------------------------
+	// pdf download by admin
+	// -----------------------------------
+	async generatedpdfService(id) {
+		let object = {};
+		object.id = id;
 
-    // Create a browser instance
-    const browser = await puppeteer.launch();
+		console.log("service==============", id);
+		const UserData = await this.usersRepository.getAllData(id);
+		const imageData = await getImages(id);
 
-    // Create a new page
-    const page = await browser.newPage();
-
-    const html = pdfTemplate(UserData, imageData[0].profile_photo);
-    await page.setContent(html, { waitUntil: "domcontentloaded" });
-
-    // To reflect CSS used for screens instead of print
-    await page.emulateMediaType("screen");
-
-    // Downlaod the PDF
-    const pdf = await page.pdf({
-      path: "./src/controllers/result.pdf",
-      margin: { top: "100px", right: "50px", bottom: "100px", left: "50px" },
-      printBackground: true,
-      format: "A4",
-    });
-
-    // Close the browser instance
-    await browser.close();
-  }
+		object.UserData = UserData;
+		object.imageData = imageData;
+		object.path = "./src/controllers/result.pdf";
+		object.pdfTemplate = pdfTemplate;
+		await this.generetePDF.generatedpdfRepo(object);
+	}
 }
 module.exports = GeneratePdf_service;
