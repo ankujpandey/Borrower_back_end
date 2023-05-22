89d6b4118b3237dc52e1971443f1645e910d3844
ankujpandey <pandeyankuj@gmail.com>
2023-05-22 16:19:40 +0530

    .

--- src/mongodb/config.js
+++ src/mongodb/config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // For Office Laptop.
-// mongoose.connect("mongodb://localhost:27017/borrower_DB");
+mongoose.connect("mongodb://localhost:27017/borrower_DB");
 
 // for ankuj personal laptop.
-mongoose.connect("mongodb://127.0.0.1:27017/borrower_DB");
+// mongoose.connect("mongodb://127.0.0.1:27017/borrower_DB");
